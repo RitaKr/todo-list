ae61e750e9d0039017c499014bf9908c2dc54188
RitaKr <ritakra2005@gmail.com>
2019-11-07 19:14:08 +0200

    added js file

---
+++ script.js
@@ -0,0 +1,18 @@
+let tasks = [
+  {
+    id: 1,
+    title: "Выучить JS",
+    text: "Ходить на курсы. Делать домашки",
+    completed: false
+  },
+  {
+    id: 2,
+    title: "Выучить React",
+    text: "Ходить на курсы. Делать домашки. Читать документацию",
+    completed: false
+  }
+];
+/*function name() {
+    console.log("hi");
+  }
+  name();*/
