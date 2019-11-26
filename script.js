7ad996cd65d13ac51458aeffb499959ce0f6d9bf
RitaKr <ritakra2005@gmail.com>
2019-11-26 19:24:00 +0200

    script developed, colors changed

--- script.js
+++ script.js
@@ -19,14 +19,35 @@ let tasks = [
     return acc;
   }, {});
   console.log("objectOfTasks", objectOfTasks);
+
   function renderAllTasks(taskList) {
     Object.values(taskList).forEach(task => {
       console.log(task);
       const li = listItem(task);
+      console.log(li);
     });
   }
   function listItem({ id, title, text }) {
-    console.log(id, title, text);
+    //console.log(id, title, text);
+    const li = document.createElement("li");
+    li.classList.add("item");
+
+    const h3 = document.createElement("h3");
+    h3.classList.add("title");
+    h3.textContent = title;
+
+    const p = document.createElement("p");
+    p.classList.add("article");
+    p.textContent = text;
+
+    const button = document.createElement("button");
+    button.textContent = "Delete task";
+    button.classList.add("btn");
+
+    li.appendChild(h3);
+    li.appendChild(p);
+    li.appendChild(button);
+    return li;
   }
   renderAllTasks(objectOfTasks);
 })(tasks);
